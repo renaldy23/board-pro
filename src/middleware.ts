b3c364f8c7b914c5f5c747e5ed4ef30653766f99
mrenaldyr <mrenaldyr03@gmail.com>
2023-12-04 08:11:55 +0700

    feat: fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,26 @@
-import { authMiddleware } from '@clerk/nextjs';
+import { authMiddleware, redirectToSignIn } from '@clerk/nextjs';
+import { NextResponse } from 'next/server';
 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
   publicRoutes: ['/', '/verify'],
+  afterAuth(auth, req) {
+    if (!auth.userId && !auth.isPublicRoute) {
+      return redirectToSignIn({ returnBackUrl: req.url });
+    }
+
+    if (
+      auth.userId &&
+      (req.nextUrl.pathname === '/register' || req.nextUrl.pathname === '/login')
+    ) {
+      const dashboard = new URL('/dashboard', req.url);
+      return NextResponse.redirect(dashboard);
+    }
+
+    return NextResponse.next();
+  },
 });
 
 export const config = {
